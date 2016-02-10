8e8bc8368bfc44202084a46a37d1501067d52b2c
Santiago Jara <swja1991@hotmail.com>
2016-02-09 19:04:33 -0500

    Cambiado la ruta a personas

--- Aplicacion/Aplicacion/App_Start/RouteConfig.cs
+++ Aplicacion/Aplicacion/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Aplicacion
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "persona", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "personas", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
